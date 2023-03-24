e3ba25c7a31b01aa74dd151985fc1fc7ba325e5f
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-03-24 14:55:08 +0530

    commit

--- routes/addEvent.js
+++ routes/addEvent.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const {
     admin_Addevent,
-    get_adminevent
+    get_adminevent,
+    admin_getone_event
+
 
 } = require("../controller/addEvent");
 
@@ -13,6 +15,8 @@ const {
 router.post("/admin/admin_Addevent", admin_Addevent);
 
 router.get("/admin/get_adminevent", get_adminevent);
+router.get("/admin/admin_getone_event", admin_getone_event);
+
 
 
 module.exports = router;
