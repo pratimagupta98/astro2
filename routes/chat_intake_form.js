74e1feff2e761bc76c42bd82d6d9b6ef910eb1a8
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2023-04-18 15:21:21 +0530

    commit

--- routes/chat_intake_form.js
+++ routes/chat_intake_form.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
- 
+
 
 const {
     add_chat_intake,
@@ -8,18 +8,21 @@ const {
     get_chat_intake,
     getone_user_chatintek,
     editContactus,
-    dlt_ChatIntek
+    dlt_ChatIntek,
+    intekListByUser
 } = require("../controller/chat_intake_form");
 
- 
- 
- router.post("/user/add_chat_intake", add_chat_intake);
- router.get("/admin/intekListByastro/:id", intekListByastro);
- router.get("/admin/get_chat_intake", get_chat_intake);
 
- router.get("/admin/getone_user_chatintek/:id",     getone_user_chatintek)
+
+router.post("/user/add_chat_intake", add_chat_intake);
+router.get("/admin/intekListByastro/:id", intekListByastro);
+router.get("/admin/intekListByUser/:id", intekListByUser);
+
+router.get("/admin/get_chat_intake", get_chat_intake);
+
+router.get("/admin/getone_user_chatintek/:id", getone_user_chatintek)
 // router.post("/admin/editContactus/:id",     editContactus);
- router.get("/admin/dlt_ChatIntek/:id",     dlt_ChatIntek)
+router.get("/admin/dlt_ChatIntek/:id", dlt_ChatIntek)
 
 
 module.exports = router;
